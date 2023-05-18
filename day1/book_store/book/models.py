217e0c41c4a1543b16d89e4b08166115a846ccc4
Yousef Adel <yousefjooo2018@gmail.com>
2023-05-18 18:53:52 +0300

    add book model

--- day1/book_store/book/models.py
+++ day1/book_store/book/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title=models.CharField(max_length=255,unique=True)
+    description=models.TextField("book description")
+    rate=models.PositiveSmallIntegerField(default=0)
+    views=models.PositiveIntegerField(default=0)
